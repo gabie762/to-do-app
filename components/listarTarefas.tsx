2f33976b84ac9bafd18a140c67975b601ede2e72
gabie762 <gabihenriques821@gmail.com>
2024-02-28 02:56:32 -0300

    help

--- components/listarTarefas.tsx
+++ components/listarTarefas.tsx
@@ -1,6 +1,14 @@
 "use client";
 
-export function ListarTarefas() {
+type ListaDeTarefas = {
+  tarefas: {
+    id: number;
+    tarefa: string;
+    completa: boolean;
+  }[]
+}
+
+export function ListarTarefas({tarefas}:ListaDeTarefas) {
   return (
     <div className="static flex bg-black min-h-[21rem] overflow-y-auto text-white">
       <ul>
